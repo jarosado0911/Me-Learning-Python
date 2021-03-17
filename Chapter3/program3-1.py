347de0993830bd4f78751cad4b6094e1eff40ed8
James Rosado <jarosado0911@gmail.com>
2021-03-16 22:04:08 -0400

    added more little programs

---
+++ Chapter3/program3-1.py
@@ -0,0 +1,8 @@
+# This program will store my friends names in a list, then I will print the list
+# Written by James Rosado 3-16-2021
+
+name = ['Brandi','Andrew','Rosie','Stephanie']
+print('Name 1: ' + name[0])
+print('Name 2: ' + name[1])
+print('Name 3: ' + name[2])
+print('Name 4: ' + name[3])
---
+++ Chapter3/program3-2.py
@@ -0,0 +1,9 @@
+#This program will print each person's name with a personal message
+# Written by James Rosado
+# 3-16-2021
+
+name = ['Brandi','Andrew','Rosie','Stephanie']
+print('Hi ' + name[0])
+print('Hello ' + name[1])
+print('Hola ' + name[2])
+print('Que Tal ' + name[3])
