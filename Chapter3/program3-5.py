8c8892ad6463fffdff900426e1b4ae2460dce894
James Rosado <jarosado0911@gmail.com>
2021-03-17 21:55:23 -0400

    added more little programs

---
+++ Chapter3/program3-5.py
@@ -0,0 +1,12 @@
+dinnerlist = ['Mom','Dad', 'Munchkin','Stephanie']
+print(dinnerlist[0] + ' I would like to invite you to dinner')
+print(dinnerlist[1] + ' would you like to join me at dinner?')
+print(dinnerlist[2] + ' I want you to eat dinner')
+print(dinnerlist[3] + ' can you join us for dinner?')
+print('\nIt looks like ' + dinnerlist[3] + ' cannot make it to dinner')
+dinnerlist[3] = 'Someone'
+print(dinnerlist[0] + ' I would like to invite you to dinner')
+print(dinnerlist[1] + ' would you like to join me at dinner?')
+print(dinnerlist[2] + ' I want you to eat dinner')
+print(dinnerlist[3] + ' can you join us for dinner?')
+
---
+++ Chapter3/program3-6.py
@@ -0,0 +1,23 @@
+dinnerlist = ['Mom','Dad', 'Munchkin','Stephanie']
+print(dinnerlist[0] + ' I would like to invite you to dinner')
+print(dinnerlist[1] + ' would you like to join me at dinner?')
+print(dinnerlist[2] + ' I want you to eat dinner')
+print(dinnerlist[3] + ' can you join us for dinner?')
+print('\nIt looks like ' + dinnerlist[3] + ' cannot make it to dinner')
+dinnerlist[3] = 'Someone'
+print(dinnerlist[0] + ' I would like to invite you to dinner')
+print(dinnerlist[1] + ' would you like to join me at dinner?')
+print(dinnerlist[2] + ' I want you to eat dinner')
+print(dinnerlist[3] + ' can you join us for dinner?')
+print('\nIt looks like we found a bigger table!')
+dinnerlist.insert(0,'Someone2')
+dinnerlist.insert(3,'Someone3')
+dinnerlist.append('Someone4')
+print(dinnerlist[0] + ' I would like to invite you to dinner')
+print(dinnerlist[1] + ' would you like to join me at dinner?')
+print(dinnerlist[2] + ' I want you to eat dinner')
+print(dinnerlist[3] + ' can you join us for dinner?')
+print(dinnerlist[4] + ' please join us for dinner')
+print(dinnerlist[5] + ' it would be nice if you would join us for dinner')
+print(dinnerlist[6] + ' eat with us!!')
+
