495e5c2d6c00a84e259682e0ddbf4ef227108bf5
James Rosado <jarosado0911@gmail.com>
2021-03-17 21:43:26 -0400

    added more python programs

---
+++ Chapter3/program3-3.py
@@ -0,0 +1,10 @@
+#Think of your favorite mode of transportation, such as motorcycle
+# or a car, and make a list that stores several examples. User your list
+# to print a series of statements abou these items, such as "I would like to own a Honda motorcycle"
+
+transportlist = ['ford gt','amtrak','suzuki','nissan','septa']
+print('I would like to buy one day a ' + transportlist[0])
+print('I have never used ' + transportlist[1])
+print('I used to own my grandpas ' + transportlist[2])
+print('My parents own a ' + transportlist[3])
+print('I use ' + transportlist[4] + ' to get from apartment to Temple University')
